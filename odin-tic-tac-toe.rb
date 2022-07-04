cefba25cd2ec1539a532b7c1cec5d3f38a9e983c
Thomas Rialland <rialland.thomas@hotmail.fr>
2022-07-04 23:41:31 +0200

    Create ruby file and start to plan

---
+++ odin-tic-tac-toe.rb
@@ -0,0 +1,9 @@
+# There must be only 2 classes : Player and Board
+# Player 1 and Player 2 are instances of Player
+# At the beginning of the game an instance of the board is created and played on
+
+class Player
+end
+
+class Board
+end
