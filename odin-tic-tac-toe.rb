a653fa98d701c5afdf304b36ef8a061fb7f0c1d2
Thomas Rialland <rialland.thomas@hotmail.fr>
2022-07-04 23:52:07 +0200

    Continue planning

--- odin-tic-tac-toe.rb
+++ odin-tic-tac-toe.rb
@@ -1,6 +1,16 @@
 # There must be only 2 classes : Player and Board
 # Player 1 and Player 2 are instances of Player
 # At the beginning of the game an instance of the board is created and played on
+# Board has methods to display the current state of the board, check if a move done
+# by an instance of the Player class is valid, modifying it's state with the move
+# of the player (could be 2 methods : valid number, available spot), checking 
+# winning condition, declaring a winner
+# player 1 plays with X, player 2 with O
+# Board is displayed like this :
+# [O][O][ ]
+# [X][O][ ]
+# [ ][ ][X]
+# Where each spot correspond to a number like on a Numpad                              
 
 class Player
 end
